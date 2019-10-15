ee0eb525742b8aaa26440ea88c0b8c7340ab7eac
Hwan-seok <tttkhs@khu.ac.kr>
2019-10-16 03:15:11 +0900

    초기 세팅

---
+++ src/components/Routes.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import {
+  Home,
+  Login,
+  Signin,
+  MyPage,
+  Rooms,
+  InsideRoom,
+  MakeRoom,
+} from '../pages';
+
+const Routes = () => (
+  <div>
+    <Route exact path="/" component={Home}></Route>
+    <Route path="/login" component={Login}></Route>
+    <Route path="/sign" component={Signin}></Route>
+    <Route path="/rooms/page/:page" component={Rooms}></Route>
+    <Route path="/room/:roomNum" component={InsideRoom}></Route>
+    <Route path="/room/makeRoom" component={MakeRoom}></Route>
+    <Route path="/myPage" component={MyPage}></Route>
+  </div>
+);
+
+export default Routes;
