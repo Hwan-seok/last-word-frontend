f55c3e71cc1bb609a93c81cde6ae93ad14d9c6ab
Hwan-seok <tttkhs@khu.ac.kr>
2019-10-20 23:54:43 +0900

    - request interceptor 작성   - 요청시 자동으로 bearer token 보내도록

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
+const NOT_AUTHORIZED_HTTP_CODE = 401;
 
 const requestAPI = axios.create();
 
 // request.defaults.baseURL = 'http://localhost:3000';
 
 requestAPI.interceptors.request.use(
-    (config) => {},
+    (config) => {
+      const accessToken = localStorage.getItem('access_token');
+      const isNotRequiredAuthURL = config.url && config.url.includes('auth');
+
+      if (accessToken && isNotRequiredAuthURL === false) {
+        config.headers.Authorization = `bearer ${accessToken}`;
+      }
+
+      return config;
+    },
     (error) => Promise.reject(error)
 );
