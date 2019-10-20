ef50a1151bdcf5f6cb316fe642e11612a571dcd1
Hwan-seok <tttkhs@khu.ac.kr>
2019-10-21 02:43:28 +0900

    - header, title, footer 임시

---
+++ src/layouts/appFooter/AppFooter.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppFooter = () => {
+  return <div></div>;
+};
+
+export default AppFooter;
---
+++ src/layouts/appHeader/AppHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppHeader = () => {
+  return <div></div>;
+};
+
+export default AppHeader;
