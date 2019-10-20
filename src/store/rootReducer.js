9ad1e995563c43d88bc6c5b8524ccb9d8f32dc03
Hwan-seok <tttkhs@khu.ac.kr>
2019-10-21 02:44:20 +0900

    - reducer 정리 - saga 준비

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
+import {connectRouter} from 'connected-react-router';
 
 import authReducer from './auth/auth.reducer';
-import {connectRouter} from 'connected-react-router';
 
 const createRootReducer = (history) =>
   combineReducers({
