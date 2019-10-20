d8368473e73fd7ba9b046ce0056dcb6957857c63
Hwan-seok <tttkhs@khu.ac.kr>
2019-10-20 23:58:06 +0900

    - router 변경 - redux-persist 사용 준비

--- src/core/App.jsx
+++ src/core/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import {ConnectedRouter} from 'connected-react-router';
 
 import history from 'utils/history';
 import {Provider} from 'react-redux';
 import configureStore from 'store/configStore';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import {AppTitle, AppHeader, AppFooter} from '../layouts';
 import Routes from './Routes';
@@ -13,12 +15,16 @@ const {store, persistor} = configureStore(initialState, history);
 
 const App = () => (
   <Provider store={store}>
-    <BrowserRouter>
-      <AppTitle></AppTitle>
-      <AppHeader></AppHeader>
-      <AppFooter></AppFooter>
-      <Routes></Routes>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <BrowserRouter>
+          <AppTitle></AppTitle>
+          <AppHeader></AppHeader>
+          <AppFooter></AppFooter>
+          <Routes></Routes>
+        </BrowserRouter>
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>
 );
 
